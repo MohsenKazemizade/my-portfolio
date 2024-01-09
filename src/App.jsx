3fb641e37b1d998fda5975bc111b51f25d3660ff
MohsenKazemizadeDev <mohsenkazemizade.dev@gmail.com>
2024-01-09 21:59:34 +0330

    initial Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import char from './assets/charector.png';
-import linkedinIcon from './assets/linkedin.svg';
-import instagram from './assets/instagram.svg';
-import github from './assets/github.svg';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,41 +11,8 @@ function App() {
       <div className='bg-black flex flex-row justify-center w-full'>
         <div className='bg-black w-[1440px] h-[1024px]'>
           <div className='relative h-[1024px] bg-[url(./assets/charrrr.png)] bg-cover bg-[50%_50%]'>
-            <div className="absolute w-[188px] top-[43px] left-[54px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[24px] tracking-[0] leading-[normal]">
-              Maison Cuz
-            </div>
-            <div className='flex w-[407px] items-center gap-[64px] absolute top-[44px] left-[752px]'>
-              <div className='inline-flex items-start gap-[48px] relative flex-[0_0_auto]'>
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
-                  Home
-                </div>
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
-                  Projects
-                </div>
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
-                  About
-                </div>
-                <div className="relative w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
-                  Contacts
-                </div>
-              </div>
-              <div className='relative flex-[0_0_auto] mr-[-88.00px]' />
-            </div>
-            <img
-              className='absolute w-[30px] h-[29px] top-[43px] left-[1234px]'
-              alt='Prime github'
-              src={github}
-            />
-            <img
-              className='absolute w-[24px] h-[24px] top-[45px] left-[1283px]'
-              alt='Akar icons linkedin'
-              src={linkedinIcon}
-            />
-            <img
-              className='absolute w-[24px] h-[24px] top-[46px] left-[1191px]'
-              alt='Instagram'
-              src={instagram}
-            />
+            <Header />
+
             <div className='absolute w-[1350px] h-[898px] top-[100px] left-[90px]'>
               <p className="absolute top-[229px] left-[12px] [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[28px] tracking-[0] leading-[normal]">
                 Hi , my name is Maison,
@@ -71,7 +36,7 @@ function App() {
                   </p>
                 </div>
                 <img
-                  className='absolute w-[861px] h-[898px] top-0 left-[489px] object-cover'
+                  className='absolute w-[861px] h-[898px] top-0 left-[450px] object-cover'
                   alt='Untitled design'
                   src={char}
                 />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,47 @@
+import linkedinIcon from '../assets/linkedin.svg';
+import instagram from '../assets/instagram.svg';
+import github from '../assets/github.svg';
 export default function Header() {
-  return <header>{/* Your header content goes here */}</header>;
+  return (
+    <header className='flex flex-row py-10 px-14'>
+      <div className='basis-1/2'>
+        <div className=" [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[24px] tracking-[0] leading-[normal]">
+          Maison Cuz
+        </div>
+      </div>
+      <div className='basis-1/2 flex flex-row pr-10 place-items-center'>
+        <div className="basis-1/6 relative text-center w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
+          Home
+        </div>
+        <div className="basis-1/6 relative text-center w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
+          Projects
+        </div>
+        <div className="basis-1/6 relative text-center w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
+          About
+        </div>
+        <div className="basis-1/6 relative text-center w-fit mt-[-1.00px] [font-family:'Poppins-Regular',Helvetica] font-normal text-color-2 text-[18px] tracking-[0] leading-[normal]">
+          Contacts
+        </div>
+        <div className='basis-2/6 flex flex-row-reverse gap-4 px-4 items-center'>
+          <div className='grow'></div>
+          <img
+            className='w-[24px] h-[24px] top-[45px] left-[1283px]'
+            alt='Akar icons linkedin'
+            src={linkedinIcon}
+          />
+          <img
+            className='w-[30px] h-[29px] top-[43px] left-[1234px]'
+            alt='Prime github'
+            src={github}
+          />
+          <img
+            className='w-[24px] h-[24px] top-[46px] left-[1191px]'
+            alt='Instagram'
+            src={instagram}
+          />
+          <div className='grow'></div>
+        </div>
+      </div>
+    </header>
+  );
 }
